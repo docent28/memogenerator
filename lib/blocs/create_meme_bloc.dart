6e4db08ee647ca23852549d0beeb7831662fbd30
docent28 <eaa67@mail.ru>
2021-11-16 12:49:14 +0300

    Сгенерировали уникальный идентификатор

--- lib/blocs/create_meme_bloc.dart
+++ lib/blocs/create_meme_bloc.dart
@@ -1,3 +1,43 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:uuid/uuid.dart';
+
 class CreateMemeBloc {
-  void dispose() {}
+  final memeTextsSubject =
+      BehaviorSubject <List<MemeText>>.seeded(<MemeText>[]);
+
+
+
+  void dispose() {
+    memeTextsSubject.close();
+  }
+}
+
+class MemeText {
+  final String id;
+  final String text;
+
+  MemeText({
+    required this.id,
+    required this.text,
+  });
+
+  factory MemeText.create() {
+    return MemeText(id: Uuid().v4(), text: "");
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MemeText &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          text == other.text;
+
+  @override
+  int get hashCode => id.hashCode ^ text.hashCode;
+
+  @override
+  String toString() {
+    return 'MemeText{id: $id, text: $text}';
+  }
 }
