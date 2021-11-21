dfb0d607c18bc176ab1229b7db2b525f8c8ff4f8
docent28 <eaa67@mail.ru>
2021-11-21 13:02:07 +0300

    Отобразили список с сохраненными мемами на MainPage

--- lib/presentation/main/main_bloc.dart
+++ lib/presentation/main/main_bloc.dart
@@ -1,3 +1,9 @@
+import 'package:memogenerator/data/models/meme.dart';
+import 'package:memogenerator/data/repositories/memes_repository.dart';
+
 class MainBloc {
+  Stream<List<Meme>> observeMemes() =>
+      MemesRepository.getInstance().observeMemes();
+
   void dispose() {}
 }
