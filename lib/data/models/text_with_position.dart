e5eccd351ca679ca1355745164616bfca529741c
docent28 <eaa67@mail.ru>
2022-01-06 22:35:18 +0300

    Изменили модель для указания размера шрифта и его цвета при записи

--- lib/data/models/text_with_position.dart
+++ lib/data/models/text_with_position.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:memogenerator/data/models/position.dart';
 
@@ -9,11 +10,15 @@ class TextWithPosition extends Equatable {
   final String id;
   final String text;
   final Position position;
+  final double fontSize;
+  final Color color;
 
   TextWithPosition({
     required this.id,
     required this.text,
     required this.position,
+    required this.fontSize,
+    required this.color,
   });
 
   factory TextWithPosition.fromJson(final Map<String, dynamic> json) =>
@@ -22,5 +27,5 @@ class TextWithPosition extends Equatable {
   Map<String, dynamic> toJson() => _$TextWithPositionToJson(this);
 
   @override
-  List<Object?> get props => [id, text, position];
+  List<Object?> get props => [id, text, position, fontSize, color];
 }
